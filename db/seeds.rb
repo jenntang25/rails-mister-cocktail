5312f0c63c01d243b60f1e2d9d29cf4035d63166
Jennifer Tang jenntangy@gmail.com <jenntangy@gmail.com>
2017-06-08 16:36:40 +0200

    It is all green

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# puts 'Cleaning database...'
+# Cocktail.destroy_all
+
+# puts 'Creating cocktails through ingredients...'
+
+Cocktail.create(name: "moscow mule")
+Cocktail.create(name: "white russian")
+Cocktail.create(name: "black russian")
+Cocktail.create(name: "gin and tonic")
+Cocktail.create(name: "screwdriver")
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "soda water")
+Ingredient.create(name: "bitters")
+Ingredient.create(name: "Coffee brandy")
+Ingredient.create(name: "Bourbon")
+Ingredient.create(name: "Irish whiskey")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Tequila")
+Ingredient.create(name: "orange juice")
+
+
+# Cocktail.create!(cocktails)
+# puts 'Finished!'
