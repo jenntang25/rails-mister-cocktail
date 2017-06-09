a9bc254de783db1070a240076aac79ca08289cd0
Jennifer Tang jenntangy@gmail.com <jenntangy@gmail.com>
2017-06-09 11:09:03 +0200

    Add figaro - Protect my secret data in config/application.yml

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,21 +5,24 @@ class DosesController < ApplicationController
   end
 
   def create
+   # @ingredient = Ingredient.find(params[:dose][:ingredient])
    @cocktail = Cocktail.find(params[:cocktail_id])
    @dose = Dose.new(dose_params)
    @dose.cocktail = @cocktail
+   # @dose.ingredient = @ingredient
     if @dose.save
       redirect_to @cocktail
     else
       render :new
     end
   end
+
   def destroy
      @dose = Dose.find(params[:id])
       @dose.destroy
 
     # no need for app/views/restaurants/destroy.html.erb
-    redirect_to cocktails_path
+    redirect_to cocktail_path(@dose.cocktail)
   end
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
       resources :doses, only: [:create, :new, :show]
     end
     resources :doses, only: [:destroy]
+    root "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
